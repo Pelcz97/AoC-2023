af72fe12af17e15fd6e01347ed6f114c78ba74aa
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2024-01-28 13:32:10 +0100

    completed & refactored part 2

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+use crate::structs::game::Game;
+
+pub mod structs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./Input2.txt");
+    let lines = input.lines();
+    let mut result = 0;
+    
+    for line in lines {
+        let game = Game::from_line(String::from(line));
+        let max_red = game.plays.iter().map(|play|play.red).max().unwrap();
+        let max_green = game.plays.iter().map(|play|play.green).max().unwrap();
+        let max_blue = game.plays.iter().map(|play|play.blue).max().unwrap();
+        result += max_red * max_blue * max_green;
+    }
+    print!("{}", result);
 }
