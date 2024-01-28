f8c9f66af3366b29287815ca8912517d49f93da8
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2024-01-28 10:26:56 +0100

    completed part1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./Input1.txt");
+    let lines = input.lines();
+    let mut result = 0;
+    for line in lines {
+        let num1 = find_first_digit(String::from(line)).unwrap();
+        let num2 = find_first_digit(line.chars().rev().collect::<String>()).unwrap();
+        result += num1 * 10 + num2;
+    }
+    print!("{}", result);
+}
+
+fn find_first_digit(line : String) -> Option<u32> {
+    for elem in line.chars() {
+        if elem.is_digit(10) {
+            return elem.to_digit(10);
+        }
+    }
+    return Option::None;
 }
